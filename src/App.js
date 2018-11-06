a98d7bf892c092e0b1f5e00865914f69b4bf7d32
JON J <gr8fuldwg@gmail.com>
2018-11-06 18:28:05 -0500

    created window alert with message to appear on click

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  handleClick(event) {
+    window.alert("You Don't Know ME!")
+  }
+  
+  
   render() {
     return (
       <div className="App-container">
         <main className="App-main">
-          <div className="App-box"></div>
+          <div className="App-box" onClick={this.handleClick} ></div>
         </main>
       </div>
     );
