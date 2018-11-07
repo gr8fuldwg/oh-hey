39469a23d5a85c3dd009b605bfebc4dc169b156b
JON J <gr8fuldwg@gmail.com>
2018-11-06 19:40:30 -0500

    Updates

---
+++ precache-manifest.7b8ba6a2eb4c87ec7df3801bf3f2ccbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec20e05fc6dd9850044fc3e110edb76b",
+    "url": "/static/media/pill.ec20e05f.jpg"
+  },
+  {
+    "revision": "1e8e6da6f345e4a86d64bd71556d8eb9",
+    "url": "/static/media/bg-img.1e8e6da6.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e36f54967bc1f02fccaa",
+    "url": "/static/js/main.e36f5496.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "e36f54967bc1f02fccaa",
+    "url": "/static/css/main.83eb1e84.chunk.css"
+  },
+  {
+    "revision": "5603471b9239de18281340f080faf833",
+    "url": "/index.html"
+  }
+];
