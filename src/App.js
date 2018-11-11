05ab0ec8b2e4c3bfd04f9940d3b4e2b8f92394f8
JON J <gr8fuldwg@gmail.com>
2018-11-11 00:27:56 -0500

    added href link to red pill

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+
+
 class App extends Component {
 
   handleClickOne(event) {
-    window.alert("Wise choice.....you have selected the red pill. Let us take a step into the reality that not all are privy to. Perhaps you should kiss your concept of what is and what isn't goodbye for your realm is on the brink of eternal change. Are you ready................................................................................................?")
+    window.alert("Wise choice.....you have selected the red pill. Let us take a step into the reality that not all are privy to. Perhaps you should kiss your concept of what-is, and, what-isn't, goodbye. For your realm is on the brink of eternal change, and the fibers of what you consider 'reality' are about to unthread beyond recognition. There is no unringing this bell. Are you ready................................................................................................?")
   }
 
   handleClickTwo(event) {
     window.alert("There is no reality.........")
-  }
-  
-  
+  };
+   
+ 
   render() {
     return (
       <div className="App-container">
         <main className="App-main">
           <div className="App-box"  >
-          <div className="App-box-1" onClick={this.handleClickOne}></div>
+          <div className="App-box-1" onClick={this.handleClickOne}><a href="https://youtube/tv#/watch?v=oi6mPsdhy-Q"><button class="btn btn-primary"></button></a></div>
           <div className="App-box-2" onClick={this.handleClickTwo}>
 
           
@@ -29,5 +31,8 @@ class App extends Component {
     );
   }
 }
+
+
+ 
 
 export default App;
